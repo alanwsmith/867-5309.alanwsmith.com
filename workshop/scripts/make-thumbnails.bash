7a88743b0429cba3ebf4f32fc40772cec821ad68
Alan Smith <commit@alanwsmith.com>
2025-01-12 09:18:56 -0500

    Reduced background image size

--- workshop/scripts/make-thumbnails.bash
+++ workshop/scripts/make-thumbnails.bash
@@ -2,5 +2,5 @@
 
 INPUT_FILE="/Users/alan/Desktop/tmp_video/input.webm"
 OUTPUT_DIR="/Users/alan/workshop/867-5309.alanwsmith.com/site/thumbs"
-ffmpeg -i "${INPUT_FILE}" -y -q:v 3 -vf fps=0.2 "${OUTPUT_DIR}/thumbnail-%d.jpg"
+ffmpeg -i "${INPUT_FILE}" -y -q:v 3 -vf "fps=0.2,scale=320:-2" "${OUTPUT_DIR}/thumbnail-%d.jpg"
 
